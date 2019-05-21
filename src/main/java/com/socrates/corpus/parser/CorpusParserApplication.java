848f1477be95729aeefe8f786ce70db1ca310248
Socrates Velasco <svelasco@xanadu.ie>
2019-05-21 17:57:47 +0200

    Initial commit

---
+++ src/main/java/com/socrates/corpus/parser/CorpusParserApplication.java
@@ -0,0 +1,13 @@
+package com.socrates.corpus.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpusParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpusParserApplication.class, args);
+	}
+
+}
