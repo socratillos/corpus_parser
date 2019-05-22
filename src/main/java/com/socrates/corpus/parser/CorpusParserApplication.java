9b588e22a6009b04b3d630d5f6457430e749f9a9
Socrates Velasco <svelasco@xanadu.ie>
2019-05-22 11:52:54 +0200

    Addition of file parsers

--- src/main/java/com/socrates/corpus/parser/CorpusParserApplication.java
+++ src/main/java/com/socrates/corpus/parser/CorpusParserApplication.java
@@ -1,13 +1,18 @@
 package com.socrates.corpus.parser;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.socrates.corpus"})
 public class CorpusParserApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CorpusParserApplication.class, args);
+		
 	}
 
 }
--- src/test/java/com/socrates/corpus/parser/CorpusParserApplicationTests.java
+++ src/test/java/com/socrates/corpus/parser/CorpusParserApplicationTests.java
@@ -1,16 +1,55 @@
 package com.socrates.corpus.parser;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.socrates.corpus.input.FileParser;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CorpusParserApplicationTests {
-
-	@Test
-	public void contextLoads() {
+	
+	private static final String path = "src/test/resources/data/corpus_test.txt";
+	
+	File file;
+	
+	@Autowired
+	private FileParser fileParser;
+	
+	@Before
+	public void setUp() {
+		file = new File(path);
 	}
-
+	
+	@Test
+	public void readFileInOneString() {
+		try {
+			String completeStringFromFile = fileParser.readFile(file);
+			assertNotNull(completeStringFromFile);
+			assertFalse(completeStringFromFile.isEmpty());
+		} catch(Throwable ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void readFileInMultipleString() {
+		try {
+			List<String> multipleLinesFromFile = fileParser.readLinesFromFile(file);
+			assertNotNull(multipleLinesFromFile);
+			assertFalse(multipleLinesFromFile.isEmpty());
+			
+		} catch(Throwable ex) {
+			ex.printStackTrace();
+		}
+	}
 }
